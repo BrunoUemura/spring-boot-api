eb8eaaeb62c291b713e7077ae70ecb2b679d71b0
BrunoUemura <bruno.h.uemura@outlook.com>
2021-06-13 14:14:31 -0300

    Finishing the api with spring boot

--- README.md
+++ README.md
@@ -1,7 +1,41 @@
 ## Spring Boot API
-An API built with Spring Boot.
+A simple API built with Spring Boot. \
+In this API you can manage students information in a database:
+- **INSERT** students information to the database.
+- **RETRIEVE** students information from the database
+- **UPDATE** students information in the database.
+- **DELETE** students information from the database.
 
 ## Technologies
 - Java
 - Spring Boot
 - PostgreSQL
+
+## Build the project
+1. Delete `target` folder.
+2. Open `Maven` and execute the `Lifecycle/install`. This will generate a new target folder with `.jar` inside it.
+3. From the terminal navigate to the `target` folder and execute the .jar file by using the following command:
+```bash
+java -jar demo-0.0.1-SNAPSHOT.jar
+```
+4. If you want to execute the project in a specific port run the following command:
+```bash
+java -jar demo-0.0.1-SNAPSHOT.jar --server.port=8081
+```
+
+## Endpoints
+- `GET` - http://localhost:8080/api/v1/student
+- `POST` - http://localhost:8080/api/v1/student \
+  It is necessary to pass the body in json like the following:
+  ```json
+  {
+  "name": "Linus Torvalds",
+  "email": "linus.torvalds@gmail.com",
+  "dob": "1969-12-28 "
+  }
+  ```
+- `PUT` - http://localhost:8080/api/v1/student/<studentId>?name=<new name>&email=<new email>
+- `DELETE` - http://localhost:8080/api/v1/student/<studentId>
+
+## Author
+- Bruno Hideki Uemura [linkedin](https://www.linkedin.com/in/bruno-hideki-uemura-918589139/), [Github](https://github.com/BrunoUemura)
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -3,9 +3,11 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -28,5 +30,34 @@ public class StudentService {
             throw new IllegalStateException("Email taken");
         }
         studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean studentExists = studentRepository.existsById(studentId);
+
+        if(!studentExists){
+            throw new IllegalStateException("Student with id " + studentId + " does not exists!");
+        }
+        studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId).orElseThrow(() -> new IllegalStateException(
+                "Student with id " + studentId + " does not exists!"
+        ));
+
+        if(name != null && name.length() > 0 && !Objects.equals(student.getName(), name)){
+            student.setName(name);
+        }
+
+        if(email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("Email taken");
+            }
+            student.setEmail(email);
+        }
     }
 }
